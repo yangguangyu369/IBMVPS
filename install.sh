c001a6226d84a4fb3457081ebb5a704b392d0d63
yangguangyu369 <56715620+yangguangyu369@users.noreply.github.com>
2021-03-25 23:14:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo "================================+====="
 echo "GMT+8 20200911 00:01 Update"
-echo "感谢 @CCChieh @不愿透露神秘大佬"
 echo "==============================="
 read -p "请输入应用程序名称:" appname
 read -p "请设置你的容器内存大小(默认256):" ramsize
@@ -21,7 +20,7 @@ echo '?> '>>index.php
 echo '<body>'>>index.php
 echo '</html>'>>index.php
 
-wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+wget https://github.com/v2ray/v2ray-core/releases/download/v4.27.5/v2ray-linux-64.zip
 unzip -d v2ray1 v2ray-linux-64.zip
 cd v2ray1
 chmod 777 *
@@ -45,12 +44,8 @@ ibmcloud cf push
 domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
 vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBMVPS","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
 cd ..
-    echo "Telegram：@bigfangfang"
-    echo "Telegram Group：https://t.me/dafangbigfang"
-    echo "Telegram Channal：https://t.me/dafangbigfangC"
-    echo ""
-    echo "YouTube IBMVPS教程：https://bit.ly/3ibq1JI"
-    echo "Thanks @CCChieh @不愿透露神秘大佬"
+    
+    echo "Thanks yang"
     echo ""
 echo 配置信息
 echo 地址: $domain
